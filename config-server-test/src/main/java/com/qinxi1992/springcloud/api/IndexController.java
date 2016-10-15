035fe6be4a0b1625d01e8b6208a7a4b50654e322
qinxi <qinxi@qinxi1992.com>
2016-10-15 11:29:30 +0800

    config-server

---
+++ config-server-test/src/main/java/com/qinxi1992/springcloud/api/IndexController.java
@@ -0,0 +1,22 @@
+package com.qinxi1992.springcloud.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by qinxi on 2016/10/15.
+ */
+@RestController
+@RequestMapping
+public class IndexController {
+
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping
+    public String index(){
+
+        return from;
+    }
+}
