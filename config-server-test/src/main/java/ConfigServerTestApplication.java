52693d5f694bbf9e9390970df88c9e9f07ef4dc5
qinxi <qinxi@qinxi1992.com>
2016-10-15 10:48:57 +0800

    config-server

--- config-server-test/src/main/java/ConfigServerTestApplication.java
+++ config-server-test/src/main/java/ConfigServerTestApplication.java
@@ -1,10 +1,12 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by qinxi on 2016/10/14.
  */
+@Configuration
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ConfigServerTestApplication {
--- config-server/src/main/java/com/qinxi1992/springcloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/qinxi1992/springcloud/ConfigServerApplication.java
@@ -1,16 +1,16 @@
 package com.qinxi1992.springcloud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by qinxi on 2016/10/14.
  */
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EnableConfigServer
-@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
