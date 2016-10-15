4bb8b86bd2158ab4a3e3949f67db3d2160a49951
qinxi <qinxi@qinxi1992.com>
2016-10-15 12:08:06 +0800

    config-server-test 完成

--- config-server-test/src/main/java/com/qinxi1992/springcloud/api/IndexController.java
+++ config-server-test/src/main/java/com/qinxi1992/springcloud/api/IndexController.java
@@ -1,6 +1,7 @@
 package com.qinxi1992.springcloud.api;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping
+@RefreshScope
 public class IndexController {
 
     @Value("${from}")
