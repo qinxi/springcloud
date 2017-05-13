83744eb52f7e74d52a1614016e740ebb45310416
qinxi <811153201@qq.com>
2017-05-13 12:09:59 +0800

    修改配置

--- eureka-test/src/main/java/com/qinxi1992/springcloud/web/Index.java
+++ eureka-test/src/main/java/com/qinxi1992/springcloud/web/Index.java
@@ -2,6 +2,7 @@ package com.qinxi1992.springcloud.web;
 
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.Resource;
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by qinxi on 2017/5/9.
@@ -27,7 +29,9 @@ public class Index {
     @RequestMapping("/test")
     public  String getSentence() {
         List<ServiceInstance> list = client.getInstances("compute-service");
-        URI u = list.get(0).getUri();
+        String u = ((EurekaDiscoveryClient.EurekaServiceInstance)list.get(0)).getInstanceInfo().getVIPAddress();
+        String host = list.get(0).getHost();
+        Map<String,String> map = list.get(0).getMetadata();
 
         return restTemplate.getForObject(u.toString()+"/add?a=1&b=2", String.class);
     }
