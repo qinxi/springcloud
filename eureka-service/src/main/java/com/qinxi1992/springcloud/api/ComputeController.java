c03dc12e18c7f781228708ef17a865c717e9521f
qinxi <811153201@qq.com>
2017-08-11 17:39:48 +0800

    修复错误.

--- eureka-service/src/main/java/com/qinxi1992/springcloud/api/ComputeController.java
+++ eureka-service/src/main/java/com/qinxi1992/springcloud/api/ComputeController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,23 +19,28 @@ public class ComputeController {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    /*@Autowired
+    private DiscoveryClient client;*/
+
     @Autowired
-    private DiscoveryClient client;
+    private Registration registration;
 
 
     @RequestMapping(value = "/add" ,method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
-        ServiceInstance instance = client.getLocalServiceInstance();
+       /* ServiceInstance instance = client.getLocalServiceInstance();*/
+        String id = registration.getServiceId();
         Integer r = a + b;
-        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        logger.info("/add, host:" + id + ", result:" + r);
         return r;
     }
 
     @RequestMapping(value = "/fail" ,method = RequestMethod.GET)
     public Integer fail(@RequestParam Integer a, @RequestParam Integer b) {
-        ServiceInstance instance = client.getLocalServiceInstance();
+        //ServiceInstance instance = client.getLocalServiceInstance();
+        String id = registration.getServiceId();
         Integer r = a / b;
-        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        logger.info("/add, host:" + id + ", result:" + r);
         return r;
     }
 }
