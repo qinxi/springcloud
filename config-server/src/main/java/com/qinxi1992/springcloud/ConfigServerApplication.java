851ee28c2e8da0f90f2ecaa7255c7f4d000eb9ca
qinxi <811153201@qq.com>
2017-11-01 19:14:42 +0800

    配置中心使用 注册中心高可用

--- config-server/src/main/java/com/qinxi1992/springcloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/qinxi1992/springcloud/ConfigServerApplication.java
@@ -2,15 +2,17 @@ package com.qinxi1992.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by qinxi on 2016/10/14.
  */
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
